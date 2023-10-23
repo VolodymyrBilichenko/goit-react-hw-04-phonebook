5275b19b7ee14e4922234d0e1582d666d6019c17
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-10-23 16:22:32 +0300

    Update App.jsx
    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import { Container } from './Container/Container';
 import { Section } from './Section/Section';
 import { Form } from './Form/Form';
@@ -11,81 +11,97 @@ function filterByCriteria(field, fieldValue) {
   return field.toLowerCase().trim().includes(fieldValue.toLowerCase().trim());
 }
 
-export class App extends Component {
-  state = {
-    contacts: contacts.contacts,
-    filter: '',
-  };
+export const App = () => {
+  // state = {
+  //   contacts: contacts.contacts,
+  //   filter: '',
+  // };
+  const [contacts, setContacts] = useState(contacts.contacts);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    // Те що знаходиться в localeStorage будемо сетити в тому випадку якщо localData і її довжина більша ніж 0
-    
+  useEffect(() => {
     const localData = localStorage.getItem('contacts');
     const localeParse = JSON.parse(localData);
-    if (localData && localeParse.length > 0)
-      this.setState({ contacts: localeParse });
-    else this.setState({ contacts: contacts.contacts });
 
-    // Якщо результат парсингу в значенні null undefined, то запишуться наші контакти.
-    // this.setState({
-    //   contacts:
-    //     JSON.parse(localStorage.getItem('contacts')) ?? contacts.contacts,
-    // });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    if (localData && localeParse.length > 0) {
+      setContacts(localeParse);
+    } else {
+      setContacts(contacts.contacts)
     }
-  }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts])
+
+  // componentDidMount() {
+  //   // Те що знаходиться в localeStorage будемо сетити в тому випадку якщо localData і її довжина більша ніж 0
+
+  //   const localData = localStorage.getItem('contacts');
+  //   const localeParse = JSON.parse(localData);
+  //   if (localData && localeParse.length > 0)
+  //     this.setState({ contacts: localeParse });
+  //   else this.setState({ contacts: contacts.contacts });
+
+  //   // Якщо результат парсингу в значенні null undefined, то запишуться наші контакти.
+  //   // this.setState({
+  //   //   contacts:
+  //   //     JSON.parse(localStorage.getItem('contacts')) ?? contacts.contacts,
+  //   // });
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.contacts.length !== this.state.contacts.length) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
 
   formDeleteHandler = contactsId => {
-    this.setState({
-      contacts: this.state.contacts.filter(
-        contact => contact.id !== contactsId
-      ),
-    });
+    // this.setState({
+    //   contacts: this.state.contacts.filter(
+    //     contact => contact.id !== contactsId
+    //   ),
+    // });
+    setContacts(contacts.filter(contact => contact.id !== contactsId))
   };
 
   formAddHandler = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    // this.setState(prevState => ({
+      // }));
+      setContacts([...contacts, newContact])
   };
 
   onFilter = evt => {
-    this.setState({
-      filter: evt.target.value,
-    });
+    // this.setState({
+    // });
+    setFilter(evt.target.value)
   };
 
-  render() {
-    const filteredContacts = this.state.contacts.filter(
-      contact =>
-        filterByCriteria(contact.name, this.state.filter) ||
-        filterByCriteria(contact.number, this.state.filter)
-    );
-    return (
-      <Container>
-        <Section title={'Phonebook'}>
-          <Form onSubmit={this.formAddHandler} contacts={this.state.contacts} />
-        </Section>
+  const filteredContacts = contacts.filter(contact =>
+      filterByCriteria(contact.name, filter) ||
+      filterByCriteria(contact.number, filter)
+  );
 
-        <Section title={'Filter'}>
-          <Filter onFilter={this.onFilter} filter={this.state.filter} />
-        </Section>
+  return (
+    <Container>
+      <Section title={'Phonebook'}>
+        <Form onSubmit={formAddHandler} contacts={contacts} />
+      </Section>
 
-        <Section title={'Contacts'}>
-          {filteredContacts.length === 0 ? (
-            <EmptyContacts />
-          ) : (
-            <Contacts
-              contacts={filteredContacts}
-              formDeleteHandler={this.formDeleteHandler}
-            />
-          )}
-        </Section>
-      </Container>
-    );
-  }
+      <Section title={'Filter'}>
+        <Filter onFilter={onFilter} filter={filter} />
+      </Section>
+
+      <Section title={'Contacts'}>
+        {filteredContacts.length === 0 ? (
+          <EmptyContacts />
+        ) : (
+          <Contacts
+            contacts={filteredContacts}
+            formDeleteHandler={formDeleteHandler}
+          />
+        )}
+      </Section>
+    </Container>
+  );
 }
